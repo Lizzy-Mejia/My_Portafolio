85fca93c40645a222693a586718d37eaa99c53d0
Elizabeth Mejía <rmejiac1@miumg.edu.gt>
2022-04-21 02:42:05 -0600

    new commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /**
  * @type {import('next').NextConfig}
  */
- 
-    module.exports = {
-        pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
-      }
-  
-  export default nextConfig
+ module.exports = {
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  ypescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+}
